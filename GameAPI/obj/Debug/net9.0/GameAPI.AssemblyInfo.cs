85d44cefd7b1fda1d00c6bc06aa398f12f3c95ee
barotraumaenjoyer <npuig.dam@institutcampalans.net>
2026-03-24 18:28:48 +0100

    forth commit

--- GameAPI/obj/Debug/net9.0/GameAPI.AssemblyInfo.cs
+++ GameAPI/obj/Debug/net9.0/GameAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb5749b0f6d2139be4e416bc51bd2de4afad804f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa469407622ba2cd0c76b72650e68c10368320eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
